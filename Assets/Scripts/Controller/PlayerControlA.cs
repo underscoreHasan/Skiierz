cf4a77d04d4f6eeeaf330cbf5655249af13f9197
Bailey Brown <bailey.jt.brown@gmail.com>
2024-11-22 23:57:14 -0800

    work continues

--- Assets/Scripts/Controller/PlayerControlA.cs
+++ Assets/Scripts/Controller/PlayerControlA.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class PlayerControlA : MonoBehaviour {
 
+    private const float YAW_SCALE = 5.0f;
+    private const float ACCEL_SCALE = 25.0f;
+
+    private Rigidbody physics;
 	void Start () {
-		
+        physics = GetComponent<Rigidbody>();
 	}
 	
-	void Update () {
-		
+	void FixedUpdate () {
+        float yaw = Input.GetAxis("Horizontal");
+        float accel = Input.GetAxis("Vertical");
+
+        // we cannot ski backwards!
+        accel = Mathf.Max(0, accel);
+
+        physics.AddTorque(0, yaw * YAW_SCALE, 0, ForceMode.Acceleration);
+
+        Vector3 moveForce = transform.rotation * (new Vector3(0, 0, accel) * ACCEL_SCALE);
+        print(moveForce);
+        physics.AddForce(moveForce, ForceMode.Acceleration);
 	}
 }
