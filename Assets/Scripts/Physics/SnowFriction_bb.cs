e8049ab066ee1347f60fe877fe1bc56a98da4a26
Bailey Brown <bailey.jt.brown@gmail.com>
2024-11-23 12:52:26 -0800

    added some primitive snow friction

--- Assets/Scripts/Physics/SnowFriction_bb.cs
+++ Assets/Scripts/Physics/SnowFriction_bb.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SnowFriction_bb : MonoBehaviour {
 
+    public Suspension_bb suspFwd;
+    public Suspension_bb suspRear;
+    public float sideFrictionFactor;
+
     private Rigidbody _phys;
 
     // Use this for initialization
@@ -19,7 +23,21 @@ public class SnowFriction_bb : MonoBehaviour {
             return;
         }
 
+        // indicate local player axis
+        Vector3 lookFwdVector = transform.rotation * Vector3.forward;
+        Vector3 lookSideVector = transform.rotation * Vector3.right;
+        Vector3 lookUpVector = transform.rotation * Vector3.up;
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + lookFwdVector * 2.0f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + lookSideVector * 1.25f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + lookUpVector * 1.5f);
 
+        // project velocity onto plane
+        Vector3 projectedVel = Vector3.ProjectOnPlane(_phys.velocity, lookUpVector);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + projectedVel * 2f);
     }
 	
 	// Update is called once per frame
@@ -29,6 +47,19 @@ public class SnowFriction_bb : MonoBehaviour {
 
     // Update is called in a fixed manner
     void FixedUpdate() {
+        
+        // snowphysics only applies when a character is grounded
+        if (!(suspFwd.isGrounded || suspRear.isGrounded)) {
+            return;
+        }
 
+        // generate relevant axis vectors and project velocity onto horizontal plane
+        Vector3 lookSideVector = transform.rotation * Vector3.right;
+        Vector3 lookUpVector = transform.rotation * Vector3.up;
+        Vector3 planarVel = Vector3.ProjectOnPlane(_phys.velocity, lookUpVector);
+
+        // depending on velocity's similarity to side vector, apply friction
+        Vector3 sideVelocity = Vector3.Project(planarVel, lookSideVector);
+        _phys.AddForce(-sideVelocity * sideFrictionFactor, ForceMode.Acceleration);
     }
 }
