1ba3a27bbaa92479b522e896ce5d888dec1a03ea
Bailey Brown <bailey.jt.brown@gmail.com>
2024-12-07 13:07:50 -0800

    updated behavior

--- Assets/Scripts/Controller/SkiierControl.cs
+++ Assets/Scripts/Controller/SkiierControl.cs
@@ -7,9 +7,12 @@ public class SkiierControl : MonoBehaviour {
     public float yawScale;
     public float accelScale;
     public float jumpChargeDownforce;
-    private float minChargeDownforce = 250f; //change for minimum jump force
-    private float maxChargeDownforce = 2000f; //change for maximum jump force
-    private float chargeSpeed = 800f;  //change for speed of jump force charging
+    public AnimationCurve chargeCurve;
+    public float minChargeDownforce = 20.0f; // change for minimum jump force
+    public float maxChargeDownforce = 300.0f; // change for maximum jump force
+    public float chargeTimeSecondsElapsed = 0.0f; // time that player has charged jump
+    public float chargeTimeSeconds = 1.0f; // time until reach max jump force
+    public float releaseUpForce = 30.0f; // on release, we do give an upwards push
 
     public Suspension suspensionFwd;
     public Suspension suspensionRear;
@@ -41,17 +44,26 @@ public class SkiierControl : MonoBehaviour {
         // jumping will spring the character down
         // TODO: fine tune jump force and speed
         if (Input.GetKey(KeyCode.Space)) { //check if space is held
-            if (jumpChargeDownforce < maxChargeDownforce) { //charge force if less than max
-                jumpChargeDownforce += Time.deltaTime * chargeSpeed;
-            } else { 
-                jumpChargeDownforce = maxChargeDownforce;
-            }
-            
-        } else if (jumpChargeDownforce > 0f) { //apply at least min force if charge is > 0
-            Vector3 downForce = transform.rotation * Vector3.down * (jumpChargeDownforce + minChargeDownforce);
-            print(downForce);
+            chargeTimeSecondsElapsed += Time.deltaTime;
+
+            jumpChargeDownforce = 
+                minChargeDownforce + 
+                chargeCurve.Evaluate(chargeTimeSecondsElapsed / chargeTimeSeconds) * 
+                (maxChargeDownforce - minChargeDownforce);
+
+            Vector3 downForce = transform.rotation * Vector3.down * jumpChargeDownforce;
             physics.AddForce(downForce, ForceMode.Acceleration);
-            jumpChargeDownforce = 0;
+        } else {
+            chargeTimeSecondsElapsed = 0.0f;
+            jumpChargeDownforce = 0.0f;
+        }
+
+        // on release space
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            // up push depends on how long player has charged
+            float upFactor = Mathf.Min(1.0f, chargeTimeSecondsElapsed / chargeTimeSeconds);
+            Vector3 upForce = transform.rotation * Vector3.up * upFactor;
+            physics.AddForce(upForce, ForceMode.Impulse);
         }
     }
 }
