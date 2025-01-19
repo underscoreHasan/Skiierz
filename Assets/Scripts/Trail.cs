6d4163f744c64ce53170a47a5c52b5d34bd9d1db
Eric Y <90221705+eric-y33@users.noreply.github.com>
2025-01-18 16:37:22 -0800

    re-implemented trail effect

---
+++ Assets/Scripts/Trail.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trail : MonoBehaviour
+{
+    public TrailRenderer trailRenderer;
+    public float detectionDistance;
+    public float trailAboveGround;
+
+    private bool _isGrounded;
+    private Vector3 distanceToGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (trailRenderer == null) {
+            Debug.LogError("missing trailRenderer!");
+        } else {
+            trailRenderer.emitting = false;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+ 
+    void FixedUpdate()
+    {
+        RaycastHit hitOut;
+        _isGrounded = Physics.Raycast(transform.position, transform.rotation * Vector3.down, out hitOut, detectionDistance);
+        
+        if (_isGrounded) {
+            distanceToGround = new Vector3(0, hitOut.distance - trailAboveGround, 0); 
+            trailRenderer.SetPosition(trailRenderer.positionCount - 1, transform.position - distanceToGround);
+            trailRenderer.emitting = true;
+        } else {
+            trailRenderer.emitting = false;
+        }
+    }
+}
