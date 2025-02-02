42b3655f6861e5600d32150318671952886c09d7
Bailey Brown <bailey.jt.brown@gmail.com>
2025-02-01 21:15:52 -0800

    minor tweaks

--- Assets/Scripts/Physics/Suspension.cs
+++ Assets/Scripts/Physics/Suspension.cs
@@ -74,11 +74,9 @@ public class Suspension : MonoBehaviour {
         Gizmos.color = Color.yellow;
         Gizmos.DrawLine(globalSpringNeutral, globalSpringNeutral + _dbgDamperForce * 0.05f);
 
-        // draw precieved axial velocity vs real velocity
+        // draw precieved axial velocity
         Gizmos.color = Color.green;
         Gizmos.DrawLine(globalSpringNeutral, globalSpringNeutral + _dbgAxialVel * 0.5f);
-        Gizmos.color = Color.white;
-        Gizmos.DrawLine(globalSpringNeutral, globalSpringNeutral + parentPhys.velocity * 0.5f);
     }
 
     // Update is called every frame
