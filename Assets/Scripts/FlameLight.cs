02c3722348d440b2ed69e1b51253692c0665449e
Bailey Brown <bailey.jt.brown@gmail.com>
2025-03-28 21:12:52 -0700

    more work on lights

--- Assets/Scripts/FlameLight.cs
+++ Assets/Scripts/FlameLight.cs
@@ -9,16 +9,17 @@ public class FlameLight : MonoBehaviour
     public float speed;
 
     private Light _light;
+    private float _randseed;
 
     private void Awake() {
         _light = GetComponent<Light>();
+        _randseed = Random.Range(-10.0f, 10.0f);
     }
 
     private void Update() {
-        float randVal = Mathf.PerlinNoise(Time.time * speed, 0.0f);
-        print(Time.time * speed);
-        print(speed);
-        print(randVal);
-        _light.intensity = Mathf.Lerp(minIntensity, maxIntensity, randVal);
+        float valLevel1 = Mathf.PerlinNoise(_randseed + Time.time * speed, 0.0f) * 0.5f;
+        float valLevel2 = Mathf.PerlinNoise(_randseed + Time.time * speed * 2.0f, 1.0f) * 0.35f;
+        float valLevel3 = Mathf.PerlinNoise(_randseed + Time.time * speed * 2.0f, 2.0f) * 0.15f;
+        _light.intensity = Mathf.Lerp(minIntensity, maxIntensity, valLevel1 + valLevel2 + valLevel3);
     }
 }
